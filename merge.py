d30eb4d8c6bfaf4c434e47e97c013c9fb3ff461d
Ethan <ethan.ledouble@epitech.digital>
2024-10-10 10:43:14 +0200

    [add] delete column

--- merge.py
+++ merge.py
@@ -1,40 +1,17 @@
 import pandas as pd
 
-def merge_csv(file1, file2, output_file):
-    df1 = pd.read_csv(file1)
-    df2 = pd.read_csv(file2)
-    merged_df = pd.merge(df1, df2)
-    merged_df.to_csv(output_file, index=False)
-    print("Données fusionnées et exportées avec succès !")
 
-file1 = 'chemin_vers_fichier1.csv'
-file2 = 'chemin_vers_fichier2.csv'
-output_file = 'chemin_vers_fichier_final.csv'
-
-merge_csv(file1, file2, output_file)
+df = pd.read_csv('Dataset/CSV_EQUIPE_ADVERSE.csv')
+colonnes_a_supprimer = [
+    'own_goals', 'opponent_manager_name', 'hosting', 'opponent_position', 'opponent_id',
+    'own_manager_name', 'url', 'filename', 'last_season', 'coach_name',
+    'net_transfer_record', 'stadium_seats', 'stadium_name', 'national_team_players',
+    'foreigners_percentage', 'foreigners_number', 'average_age', 'squad_size',
+    'total_market_value', 'club_code', 'confederation', 'description',
+    'player_in_id', 'player_assist_id', 'type', 'team_captain'
+]
 
 
+df = df.drop(columns=colonnes_a_supprimer)
 
-
-# import pandas as pd
-# from pathlib import Path
-
-# def load_data(filename, columns):
-#     data_vva = Path() / f'Dataset_Clean/{filename}'
-#     data = pd.read_csv(data_vva, usecols=columns)
-#     print("Données chargées avec succès !")
-#     return data
-
-
-# df1 = pd.read_csv('Dataset_Clean/VVA_f1_data_modified7.csv')
-# df2 = pd.read_csv('Dataset_Clean/VVA_weather.csv')
-# df2 = df2.rename(columns={'weatherLocation': 'racename', 'weatherYear': 'year'})
-# df2 = df2.round(2)
-
-# df3 = pd.merge(df1, df2, on=['racename', 'year'])
-
-# df3.to_csv('VVA_f1_data_final.csv', index=False)
-
-# print("Données fusionnées et exportées avec succès !")
-
-#merged_df = pd.merge(df1, df2, on=['racename', 'year']
+df.to_csv('Dataset/CSV_EQUIPE_ADVERSE.csv', index=False)
